ba3cb6760771e545d2a55689a17a579eceed0270
Grant Kim <grantkim93@gmail.com>
2018-02-03 15:53:30 -0500

    final for demo

--- public/controllers/friend.js
+++ public/controllers/friend.js
@@ -1,6 +1,27 @@
-$("#person1").on("click", function() {
+$("#person1").on("mouseenter", function() {
+    console.log("clicked");
     for(var i=0; i<myAccessories.length; i++) {
         document.getElementById(accessories[myAccessories[i]].id).style.visibility = "hidden";
 
     }
+
+    document.getElementById(accessories[4].id).style.visibility = "visible"
+    document.getElementById(accessories[5].id).style.visibility = "visible"
+    document.getElementById(accessories[3].id).style.visibility = "visible"
+
+    NAME.innerHTML = "Stacy's Mom";
+
+});
+
+$("#person1").on("mouseleave", function() {
+    document.getElementById(accessories[4].id).style.visibility = "hidden"
+    document.getElementById(accessories[5].id).style.visibility = "hidden"
+    document.getElementById(accessories[3].id).style.visibility = "hidden"
+
+    for(var i=0; i<myAccessories.length; i++) {
+        document.getElementById(accessories[myAccessories[i]].id).style.visibility = "visible";
+
+    }
+
+    NAME.innerHTML = name;
 });
