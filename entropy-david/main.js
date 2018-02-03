9e085eecc42560b20b00360abbf40f6b1126a378
David Cheng <dc273@duke.edu>
2018-02-03 01:48:25 -0500

    Images

---
+++ entropy-david/main.js
@@ -0,0 +1,52 @@
+const SICK = document.getElementById("background");
+const TIME = document.getElementById("time");
+const WEEKDAY = document.getElementById("weekday");
+
+var blink = true;
+
+var d = new Date();
+var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var hours = ["12", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11"];
+
+
+// Hide scrollbars and disable scrolling
+$("body").css("overflow", "hidden");
+
+function changeHealth(health) {
+  SICK.style.filter = "saturate(" + (health) + "%)";
+}
+
+function updateTime() {
+  var d = new Date();
+  var time = hours[d.getHours()] ;
+
+  if(blink) {
+    time += ":";
+    blink = false;
+  } else {
+    time += " ";
+    blink = true;
+  }
+
+  if(d.getMinutes() < 10) {
+    time += "0" + d.getMinutes();
+  } else {
+    time += d.getMinutes();
+  }
+
+  if(d.getHours > 12) {
+    time += " PM";
+  } else {
+    time += " AM";
+  }
+
+  WEEKDAY.innerHTML = weekday[d.getDay()];
+
+  TIME.innerHTML = time;
+}
+
+changeHealth(70)
+
+
+updateTime();
+setInterval(updateTime, 1000);
