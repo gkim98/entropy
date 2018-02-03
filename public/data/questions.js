438f7a7a2a8025e3f2dd236796370bd7fa9b8b15
David Cheng <dc273@duke.edu>
2018-02-03 04:18:08 -0500

    Data

---
+++ public/data/questions.js
@@ -0,0 +1,21 @@
+var dailyQuestions = [
+  "Did you recycle today?",
+  "Did you turn off the water while you brushed your teeth?",
+  "Did you take less than 10 minutes to shower?",
+  "Did you litter today?",
+  "Did you turn off your computer?",
+  "Did you use a refillable water bottle instead of plastic water bottles?",
+  "Did you try to use a little paper as possible?",
+  "Did you carpool instead of drive alone recently?",
+  "Did you use the stairs instead of take the elevator?",
+  "Did you turn off the lights when you left your room?",
+  "Did you wash clothes in cold water?"
+];
+
+var goals = [
+  "Replace your lightbulbs with LEDs",
+  "Install a programmable thermostat",
+  "Use low flow faucets and shower heads",
+  "Check home for insulation/water leaks",
+  "Start a compost bin"
+];
