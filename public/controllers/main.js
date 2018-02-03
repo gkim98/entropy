6135a21190e564659520b44cdb79c30bc580f04d
Grant Kim <grantkim93@gmail.com>
2018-02-03 03:31:40 -0500

    button functionality

--- public/controllers/main.js
+++ public/controllers/main.js
@@ -1,20 +1,69 @@
-const SICK = document.getElementById("background");
+const BG = document.getElementById("background");
 const TIME = document.getElementById("time");
 const WEEKDAY = document.getElementById("weekday");
+const PLANT = document.getElementById("plant");
 
+
+var health = 50;
+setHealth(50);
 var blink = true;
 
 var d = new Date();
 var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var hours = ["12", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11"];
 
+$(".goodButton").on("click", function() {
+    $(this).parent().parent().fadeOut();
+    startHealthUp();
+});
 
-// // Hide scrollbars and disable scrolling
-// $("body").css("overflow", "hidden");
+$(".badButton").on("click", function() {
+    $(this).parent().parent().fadeOut();
+    startHealthDown();
+});
 
-function changeHealth(health) {
-  SICK.style.filter = "saturate(" + (100 - health) + "%)";
+// Hide scrollbars and disable scrolling
+$("body").css("overflow", "hidden");
+
+function setHealth(health) {
+  BG.style.filter = "saturate(" + (health) + "%)";
 }
+
+function healthUp() {
+  health += 0.2;
+  setHealth(health)
+
+  if(health == original + 50) {
+    clearInterval(myVar);
+
+  }
+}
+
+var original;
+
+function startHealthUp() {
+  original = health;
+  PLANT.src = "/images/plant/happy.png";
+  myVar = setInterval(healthUp, 1);
+}
+
+function healthDown() {
+  health -= 0.2;
+  setHealth(health)
+
+  if(health == original - 50) {
+    clearInterval(myVar);
+  }
+}
+
+function startHealthDown() {
+  original = health;
+  PLANT.src = "/images/plant/sad.png";
+  myVar = setInterval(healthDown, 1);
+}
+
+
+
 
 function updateTime() {
   var d = new Date();
@@ -44,6 +93,7 @@ function updateTime() {
 
   TIME.innerHTML = time;
 }
+
 
 updateTime();
 setInterval(updateTime, 1000);
