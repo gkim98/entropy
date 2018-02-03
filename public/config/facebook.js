1db7d3d0d63fb5d958f69e9f7596a90bfbceac25
Grant Kim <grantkim93@gmail.com>
2018-02-03 06:26:04 -0500

    setup

--- public/config/facebook.js
+++ public/config/facebook.js
@@ -1,28 +1,36 @@
-var loggedIn = false;
+// var loggedIn = false;
+//
+// window.fbAsyncInit = function() {
+//     FB.init({
+//       appId      : '1548241081963251',
+//       cookie     : true,
+//       xfbml      : true,
+//       version    : 'v2.12'
+//     });
+//
+//     FB.AppEvents.logPageView();
+//
+//     FB.getLoginStatus(function(response) {
+//         if(response.status == 'connected') {
+//             loggedIn = true;
+//             $("#facebook").text("Logout");
+//         }
+//     });
+//
+//   };
+//
+//   (function(d, s, id){
+//      var js, fjs = d.getElementsByTagName(s)[0];
+//      if (d.getElementById(id)) {return;}
+//      js = d.createElement(s); js.id = id;
+//      js.src = "https://connect.facebook.net/en_US/sdk.js";
+//      fjs.parentNode.insertBefore(js, fjs);
+//    }(document, 'script', 'facebook-jssdk'));
 
-window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '1548241081963251',
-      cookie     : true,
-      xfbml      : true,
-      version    : 'v2.12'
-    });
 
-    FB.AppEvents.logPageView();
-
-    FB.getLoginStatus(function(response) {
-        if(response.status == 'connected') {
-            loggedIn = true;
-            $("#facebook").text("Logout");
-        }
-    });
-
-  };
-
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "https://connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
+FB.init({
+    appId      : '1548241081963251',
+    status     : true,
+    xfbml      : true,
+    version    : 'v2.12'
+});
